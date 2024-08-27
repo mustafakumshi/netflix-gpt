cf088290884636f42d0f0f982d1355f5be151f50
mustafakumshi <kumshimustafa0@gmail.com>
2024-08-28 00:51:07 +0530

    Get Data from TMDB now playing movie list API

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZGI3ZDFkYmU1MzlhYzM5YTk3MzhhZWJmZmY4MzUxNSIsIm5iZiI6MTcyNDc4NDM2My43MzQxMjksInN1YiI6IjY2Y2I5MjZjMDVhMTZkMDlkMDEwM2FmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1GVSdRIUqtvysgI-3OSt-9Dfjv_RxAQgujXHj8jq-uk'
+    }
+};
