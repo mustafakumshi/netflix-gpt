85705a520adcbe2525da0963b9f981ccdb564675
mustafakumshi <kumshimustafa0@gmail.com>
2024-09-08 22:24:13 +0530

    Main Container for Browse Page Done

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants"
+import { addVideoTrailer } from "../utils/moviesSlice";
+import { useDispatch } from "react-redux";
+
+const useMovieTrailer = (movieId) => {
+
+    const dispatch = useDispatch();
+
+    const getMovieVideos = async () => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/' + movieId + '/videos?language=en-US', API_OPTIONS);
+        const json = await data.json();
+
+        const filterData = json.results.filter(video => video.type === "Trailer");
+        const trailer = filterData.length ? filterData[0] : json.results[0];
+        dispatch(addVideoTrailer(trailer));
+    }
+
+    useEffect(() => {
+        getMovieVideos();
+    }, [])
+}
+
+export default useMovieTrailer
