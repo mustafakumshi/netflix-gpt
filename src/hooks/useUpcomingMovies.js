51f6bf73efc0802570e42085b7bbf5ea79e57010
mustafakumshi <kumshimustafa0@gmail.com>
2024-09-17 00:16:09 +0530

    fixed bug for upcoming movies

--- src/hooks/useUpcomingMovies.js
+++ src/hooks/useUpcomingMovies.js
@@ -13,11 +13,11 @@ const useUpcomingMovies = () => {
   const getUpcomingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', API_OPTIONS);
     const json = await data.json();
-    !upcomingMovies && dispatch(addUpcomingMovies(json.results));
+    dispatch(addUpcomingMovies(json.results));
   }
 
   useEffect(() => {
-   getUpcomingMovies();
+  !upcomingMovies && getUpcomingMovies();
   }, []);
 
 };
