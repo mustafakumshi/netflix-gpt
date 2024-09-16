272920a4b85b4bb8d8d81a6d4b649c7a1846c69f
mustafakumshi <kumshimustafa0@gmail.com>
2024-09-16 14:40:00 +0530

    Fixed Form Submit Bug

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -6,7 +6,7 @@ const GptSearchBar = () => {
   const selectedLanguage = useSelector(store => store.config.lang);
   return (
     <div className="pt-[10%] flex justify-center">
-      <form action="" className="w-1/2 bg-black grid grid-cols-12">
+      <form action="" className="w-1/2 bg-black grid grid-cols-12" onSubmit={(e) => e.preventDefault()}>
         <input type="text" className="p-4 m-4 col-span-9" placeholder={lang[selectedLanguage].gptSearchPlaceholder} />
         <button className="py-2 px-4 m-4 col-span-3 bg-red-700 text-white rounded-lg">{lang[selectedLanguage].search}</button>
       </form>
